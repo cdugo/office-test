c7f6416c047e3bfcd3c01e0ffa0fad53f42bef92
Carlo D'Ugo <54993517+cdugo@users.noreply.github.com>
2023-06-25 14:19:21 -0400

    Add files via upload

---
+++ office-test/office-test/PowerPointService.cs
@@ -0,0 +1,45 @@
+﻿using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using System.Runtime.InteropServices;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using PowerPointOpener;
+using System.Threading.Tasks;
+
+namespace office_test
+{
+    public class PowerPointService : PowerPointOpener.PowerPointBase
+    {
+        private readonly ILogger<PowerPointService> _logger;
+        public PowerPointService(ILogger<PowerPointService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<OpenSlideReply> OpenSlide(OpenSlideRequest request, ServerCallContext context)
+        {
+            // Call your OpenPowerPointAtSlide method here
+            OpenPowerPointAtSlide(request.FilePath, request.SlideNumber);
+
+            return new OpenSlideReply
+            {
+                Message = "PowerPoint opened successfully."
+            };
+        }
+
+        // Your OpenPowerPointAtSlide method should go here
+        void OpenPowerPointAtSlide(string filePath, int slideNumber)
+        {
+            var app = new PowerPoint.Application();
+            app.Visible = Microsoft.Office.Core.MsoTriState.msoTrue;
+
+            var pres = app.Presentations.Open(filePath, WithWindow: Microsoft.Office.Core.MsoTriState.msoTrue);
+
+            // Navigate to the slide
+            app.ActiveWindow.View.GotoSlide(slideNumber);
+
+            // Clean up the COM objects
+            Marshal.ReleaseComObject(pres);
+            Marshal.ReleaseComObject(app);
+        }
+    }
+}
---
+++ office-test/office-test/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace office_test
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<PowerPointService>();
+            });
+        }
+    }
+
+}
